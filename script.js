4588ba9d754e5271f10c48ea17ce72da86966b31
Julia Volkova <juliavolk96@gmail.com>
2024-12-18 14:23:07 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Enable Bootstrap Validation
+document.addEventListener("DOMContentLoaded", function () {
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+});
